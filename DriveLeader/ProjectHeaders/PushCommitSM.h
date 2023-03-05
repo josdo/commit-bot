f9f733d2a0c1f8a3ce9211dbbf70e84135131b2a
Gandharv1412 <gandharv@stanford.edu>
2023-03-04 20:09:35 -0800

    forward a bit to enter the middle branch

--- DriveLeader/ProjectHeaders/PushCommitSM.h
+++ DriveLeader/ProjectHeaders/PushCommitSM.h
@@ -5,7 +5,8 @@ typedef enum {
     BACK_UP_A_BIT,
     ROTATE_TO_FACE_BRANCH,
             STOP_PUSH_COMMIT,
-            MOVE_FORWARD
+            MOVE_FORWARD,
+            FORWARD_A_BIT
 } PushCommitSMState_t;
 
 ES_Event_t RunPushCommitSM(ES_Event_t CurrentEvent);
--- DriveLeader/ProjectSource/PushCommitSM.c
+++ DriveLeader/ProjectSource/PushCommitSM.c
@@ -4,12 +4,14 @@
 #include "BeaconSensor.h"
 #include "DistanceSensor.h"
 #include "PushCommitSM.h"
-
+#include "GoToBranchOriginSM.h"
 
 static PushCommitSMState_t CurrentState;
 static ES_Event_t DuringRotateToFaceBranch(ES_Event_t Event);
 static ES_Event_t DuringMoveForward(ES_Event_t Event);
 static ES_Event_t DuringBackUpABit(ES_Event_t Event);
+static ES_Event_t DuringForwardABit(ES_Event_t Event);
+
 
 ES_Event_t RunPushCommitSM(ES_Event_t CurrentEvent)
 {
@@ -23,6 +25,23 @@ ES_Event_t RunPushCommitSM(ES_Event_t CurrentEvent)
         case BACK_UP_A_BIT:
         {
             CurrentEvent = DuringBackUpABit(CurrentEvent);
+            if(CurrentEvent.EventType != ES_NO_EVENT)
+            {
+                switch(CurrentEvent.EventType)
+                {
+                    case ES_TIMEOUT:
+                    {
+                        NextState = ROTATE_TO_FACE_BRANCH;
+                        MakeTransition = true;
+                    }
+                }
+            }
+        }
+        break;
+        
+        case FORWARD_A_BIT:
+        {
+            CurrentEvent = DuringForwardABit(CurrentEvent);
             if(CurrentEvent.EventType != ES_NO_EVENT)
             {
                 switch(CurrentEvent.EventType)
@@ -95,7 +114,16 @@ void StartPushCommitSM( ES_Event_t CurrentEvent )
 {
     if (ES_ENTRY_HISTORY != CurrentEvent.EventType)
     {
-        CurrentState = BACK_UP_A_BIT;
+        GoToBranchOriginState_t CurrentBranch;
+        
+        if(CurrentBranch == BRANCH_THREE)
+        {
+            CurrentState = BACK_UP_A_BIT;
+        }
+        else if(CurrentBranch == BRANCH_TWO)
+        {
+            CurrentState = FORWARD_A_BIT;
+        }
     }
     RunPushCommitSM(CurrentEvent);
 }
@@ -137,7 +165,7 @@ static ES_Event_t DuringRotateToFaceBranch(ES_Event_t Event)
     {
         setMotorSpeed(LEFT_MOTOR, BACKWARD, 30);
         setMotorSpeed(RIGHT_MOTOR, FORWARD, 30);
-        ES_Timer_InitTimer(TURN_90_TIMER, 1525);
+        ES_Timer_InitTimer(TURN_90_TIMER, 1475);
     }
     
     else if (Event.EventType == ES_EXIT)
@@ -145,6 +173,28 @@ static ES_Event_t DuringRotateToFaceBranch(ES_Event_t Event)
         setMotorSpeed(LEFT_MOTOR, FORWARD, 0);
         setMotorSpeed(RIGHT_MOTOR, FORWARD, 0);
         ES_Timer_InitTimer(STOP_TIMER, 1000);
+    }
+    else
+    {
+        
+    }
+    return ReturnEvent;
+}
+
+static ES_Event_t DuringForwardABit(ES_Event_t Event)
+{
+    ES_Event_t ReturnEvent = Event;
+    if ( (Event.EventType == ES_ENTRY) || 
+         (Event.EventType == ES_ENTRY_HISTORY))
+    {
+        setMotorSpeed(LEFT_MOTOR, FORWARD, 30);
+        setMotorSpeed(RIGHT_MOTOR, FORWARD, 30);
+        ES_Timer_InitTimer(STOP_TIMER, 500);
+    }
+     else if (Event.EventType == ES_EXIT)
+    {
+        setMotorSpeed(LEFT_MOTOR, FORWARD, 0);
+        setMotorSpeed(RIGHT_MOTOR, FORWARD, 0);
     }
     else
     {
@@ -173,3 +223,4 @@ static ES_Event_t DuringMoveForward(ES_Event_t Event)
     }
     return ReturnEvent;
 }
+
